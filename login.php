13b55b236da1c829f049dd3ced0ddaf5d8f6aa8f
yusraSenem <36928062+yusraSenem@users.noreply.github.com>
2019-04-30 23:15:33 +0300

    tab düzenlemesi
    tab düzenlemesi

--- login.php
+++ login.php
@@ -92,50 +92,7 @@
               </div>
             </div>
 
-              <div id="pk" class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image">
-                <img src="login_image.jpg" style="">
-              </div>
-              <div class="col-lg-6">
 
-                <div class="p-5">
-                  <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Giriş Ekranı</h1>
-                  </div>
-                  <form class="user" method="post" action="kontrol.php">
-                    <div class="form-group">
-                      <input type="email" class="form-control form-control-user" id="giris_eposta" name="giris_eposta" aria-describedby="emailHelp" placeholder="E-posta adresinizi giriniz">
-                    </div>
-                    <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="giris_sifre" name="giris_sifre" placeholder="Şifrenizi giriniz">
-                    </div>
-                    <div class="form-group">
-                      <div class="custom-control custom-checkbox small">
-                        <input type="checkbox" class="custom-control-input" id="customCheck">
-                        <label class="custom-control-label" for="customCheck">Remember Me</label>
-                      </div>
-                    </div>
-                    <input type="submit" class="btn btn-primary btn-user btn-block" name="giris_gonder">
-                     
-                    </input>
-                    <hr>
-                    <a href="index.html" class="btn btn-google btn-user btn-block">
-                      <i class="fab fa-google fa-fw"></i> Login with Google
-                    </a>
-                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                    </a>
-                  </form>
-                  <hr>
-                  <div class="text-center">
-                    <a class="small" href="forgot_password.html">Forgot Password?</a>
-                  </div>
-                  <div class="text-center">
-                    <a class="small" href="register_first.php">Create an Account!</a>
-                  </div>
-                </div>
-              </div>
-            </div>
            </div>
           </div>
         </div>
