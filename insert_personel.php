82a065511097192a42d9aa025b7cd0a99a3229e4
yusraSenem <36928062+yusraSenem@users.noreply.github.com>
2019-05-06 00:36:11 +0300

    insert işlemi
    veritabanına veriler insert edildi

---
+++ insert_personel.php
@@ -0,0 +1,33 @@
+<?php
+
+if(isset($_POST["ekle"])){
+  $pers_veri=[
+
+  $ad=strip_tags(trim($_POST["p_ad"])),
+  $soyad=strip_tags(trim($_POST["p_soyad"])),
+  $gsm=strip_tags(trim($_POST["gsmp"])),
+  $eposta=strip_tags(trim($_POST["p_email"])),
+  $sifre=strip_tags(trim($_POST["p_sifre"])),
+  $unvan=strip_tags(trim($_POST["p_unvan"]))
+];
+
+  require("vt_baglanti.php");
+  $sorgu=$db->prepare("INSERT INTO personeller VALUES (null, ?,?, ?,?,?,null,?,2)");
+  $eklemek=$sorgu->execute($pers_veri);
+  if($eklemek){
+    $id=$db->lastInsertId();
+    echo "işlem başarılı";
+  }
+  else{
+    echo "ekleme işlemi başarısız.";
+  }
+
+
+}
+
+else{
+  echo "post edilmedi.";
+}
+
+
+?>
--- personel_gorev_ekle.php
+++ personel_gorev_ekle.php
@@ -1,36 +1,44 @@
 <?php include "sidebar.php" ?>
 
 <div class="container-fluid">
-  <form method="post" action="">
+  <form method="post" action="insert_personel.php">
     <h2>Personel Bilgileri</h2>
     <div class="form-group">
       <label for="p_ad">Personelin Adı</label>
-      <input type="text" class="form-control" id="p_ad" placeholder="Personelin adını giriniz.">
+      <input type="text" class="form-control" id="p_ad" name="p_ad" placeholder="Personelin adını giriniz." required="">
     </div>
+
     <div class="form-group">
       <label for="p_soyad">Personelin Soyadı</label>
-      <input type="text" class="form-control" id="p_soyad" placeholder="Personelin soyadını giriniz.">
+      <input type="text" class="form-control" id="p_soyad" name="p_soyad" placeholder="Personelin soyadını giriniz." required="">
     </div>
-    <div class="form-group">
-      <label for="p_unvan">Personelin Unvanı</label>
-      <input type="text" class="form-control" id="p_unvan" placeholder="Personelin ünvanını giriniz.">
-    </div>
+
     <div class="form-group">
       <label for="gsmp">GSM</label>
-      <input type="text" class="form-control" id="gsmp" placeholder="Telefon numarasını giriniz.">
+      <input type="text" class="form-control" id="gsmp" name="gsmp" placeholder="Telefon numarasını giriniz." required="">
     </div>
+
     <div class="form-group">
       <label for="p_email">E-Posta Adresi</label>
-      <input type="email" class="form-control" id="p_email" aria-describedby="emailHelp" placeholder="E-posta">
+      <input type="email" class="form-control" id="p_email" name="p_email" aria-describedby="emailHelp" placeholder="E-posta" required="">
     </div>
+
     <div class="form-group">
       <label for="p_sifre">Şifre</label>
-      <input type="password" class="form-control" id="p_sifre" placeholder="Şifre">
+      <input type="password" class="form-control" id="p_sifre" name="p_sifre" placeholder="Şifre" required="">
     </div>
+
+    <div class="form-group">
+      <label for="p_unvan">Personelin Unvanı</label>
+      <input type="text" class="form-control" id="p_unvan" name="p_unvan" placeholder="Personelin ünvanını giriniz." required="">
+    </div>
+    
   
-    <button type="submit" class="btn btn-primary">Ekle</button>
+    <button type="submit" class="btn btn-primary" name="ekle">Ekle</button>
   </form>
 </div>
+
+
 
 
 
