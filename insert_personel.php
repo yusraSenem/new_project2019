0777926c52680e6d80e744bf723cab268d82c747
yusraSenem <36928062+yusraSenem@users.noreply.github.com>
2019-05-06 20:36:53 +0300

    gorev ekleme
    kullanıcıdan görev bilgileri alınıp veri tabanına eklendi

--- insert_personel.php
+++ insert_personel.php
@@ -31,4 +31,36 @@ else{
 }
 
 
+
+if(isset($_POST["gorev_ekle"])){
+  $gorev_veri=[
+
+  $gorev=strip_tags(trim($_POST["gorev"])),
+  $gorev_ad=strip_tags(trim($_POST["gorev_ad"])),
+  $tanim=strip_tags(trim($_POST["tanim"])),
+  $baslama=strip_tags(trim($_POST["baslama"])),
+  $bitis=strip_tags(trim($_POST["bitis"]))
+];
+
+  require("vt_baglanti.php");
+  $sorgu_gorev=$db->prepare("INSERT INTO gorevler VALUES (?,?, ?,?,?)");
+  $eklemek_gorev=$sorgu_gorev->execute($gorev_veri);
+  if($eklemek_gorev){
+    
+    echo "işlem başarılı";
+  }
+  else{
+    echo "ekleme işlemi başarısız.";
+  }
+
+
+}
+
+else{
+  echo "post edilmedi.";
+}
+
+
+
+
 ?>
--- personel_gorev_ekle.php
+++ personel_gorev_ekle.php
@@ -48,41 +48,32 @@
   <form method="post" action="insert_personel.php">
     <h2>Görev Ekle</h2>
     <div class="form-group">
-      <label for="p_ad">Personelin Adı</label>
-      <input type="text" class="form-control" id="p_ad" name="p_ad" placeholder="Personelin adını giriniz." required="">
+      <label for="gorev">Görev Kod</label>
+      <input type="text" class="form-control" id="gorev" name="gorev" placeholder="Görev kodunu giriniz." required="">
     </div>
 
     <div class="form-group">
-      <label for="p_soyad">Personelin Soyadı</label>
-      <input type="text" class="form-control" id="p_soyad" name="p_soyad" placeholder="Personelin soyadını giriniz." required="">
+      <label for="gorev_ad">Görev Adı</label>
+      <input type="text" class="form-control" id="gorev_ad" name="gorev_ad" placeholder="Görev adını giriniz." required="">
     </div>
 
     <div class="form-group">
-      <label for="gsmp">GSM</label>
-      <input type="text" class="form-control" id="gsmp" name="gsmp" placeholder="Telefon numarasını giriniz." required="">
+      <label for="tanim">Görev Tanımı</label>
+      <input type="text" class="form-control" id="tanim" name="tanim" placeholder="Görev tanımını giriniz." required="">
     </div>
 
     <div class="form-group">
-      <label for="p_email">E-Posta Adresi</label>
-      <input type="email" class="form-control" id="p_email" name="p_email" aria-describedby="emailHelp" placeholder="E-posta" required="">
+      <label for="baslama">Baslama Tarihi</label>
+      <input type="date" class="form-control" id="baslama" name="baslama" aria-describedby="emailHelp" placeholder="Görev başlangıç tarihi" required="">
     </div>
 
     <div class="form-group">
-      <label for="p_sifre">Şifre</label>
-      <input type="password" class="form-control" id="p_sifre" name="p_sifre" placeholder="Şifre" required="">
-    </div>
-
-    <div class="form-group">
-      <label for="p_unvan">Personelin Unvanı</label>
-      <input type="text" class="form-control" id="p_unvan" name="p_unvan" placeholder="Personelin ünvanını giriniz." required="">
+      <label for="bitis">Bitis Tarihi</label>
+      <input type="date" class="form-control" id="bitis" name="bitis" placeholder="Görev bitiş tarihi" required="">
     </div>
     
-    <div class="form-group">
-      <label for="bolum_k">Personelin Unvanı</label>
-      <input type="text" class="form-control" id="bolum_k" name="bolum_k" placeholder="Birim kodunu giriniz." required="">
-    </div>
   
-    <button type="submit" class="btn btn-primary" name="ekle">Ekle</button>
+    <button type="submit" class="btn btn-primary" name="gorev_ekle">Ekle</button>
   </form>
 </div>
 
