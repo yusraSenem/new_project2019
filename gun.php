28c82fbb5a874cc9a1d89b7ba02c0d2e0218c674
yusraSenem <36928062+yusraSenem@users.noreply.github.com>
2019-05-17 23:55:35 +0300

    php code fixed
    sql update komutları güncellendi

--- gun.php
+++ gun.php
@@ -1,8 +1,6 @@
 <?php
 
 if(isset($_POST["p_gun"])){
-
-	$upd=$_POST["personel_g"];
 	 $pers_veri=[
 
   $ad=strip_tags(trim($_POST["p_ad"])),
@@ -11,20 +9,33 @@ if(isset($_POST["p_gun"])){
   $eposta=strip_tags(trim($_POST["p_email"])),
   $sifre=strip_tags(trim($_POST["p_sifre"])),
   $bolum=strip_tags(trim($_POST["bolum_k"])),
-  $unvan=strip_tags(trim($_POST["p_unvan"]))
+  $unvan=strip_tags(trim($_POST["p_unvan"])),
+  $upd=$_POST["personel_g"]
+  
 ];
 
 
+
 	require("vt_baglanti.php");
-	$sorgu=$db->prepare("");
-	
+	$sorgu=$db->prepare("UPDATE personeller SET p_ad=?, p_soyad=?, p_gsm=?, p_email=?, sifre=?, bolum_kod=?, unvan=? WHERE p_id=?" );
+	$guncelle=$sorgu->execute($pers_veri);
+	if($guncelle){
+
+		echo "başarıyla güncellendi.";
+
+	}
+	else{
+		echo "sorguda hata var.";
+	}
 
 
 
 
 
 }
-
+else{
+	echo "veriler gelmedi";
+}
 
 
 
